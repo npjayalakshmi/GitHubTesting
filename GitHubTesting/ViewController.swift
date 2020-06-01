29bbcbeced55649f2adbde8ed4898b58055bcde3
Natarajan k <natraj1990k@gmail.com>
2020-06-01 17:41:31 +0530

    implement print function

--- GitHubTesting/ViewController.swift
+++ GitHubTesting/ViewController.swift
@@ -13,8 +13,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        printName()
     }
-
+    func printName(){
+        print("my github testing project")
+    }
 
 }
 
