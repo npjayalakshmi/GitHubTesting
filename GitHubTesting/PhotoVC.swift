ee4d68d7a428b28cd85fe69ef3b2b2e25ad29340
Natarajan k <natraj1990k@gmail.com>
2020-06-01 17:44:02 +0530

    Created new vc and named it photovc

---
+++ GitHubTesting/PhotoVC.swift
@@ -0,0 +1,30 @@
+//
+//  PhotoVC.swift
+//  GitHubTesting
+//
+//  Created by Jayalakshmi NP on 01/06/20.
+//  Copyright © 2020 natarajan k. All rights reserved.
+//
+
+import UIKit
+
+class PhotoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
